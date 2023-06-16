9839280192a60770407d63b4e1a9bb4310bc03dd
aniketkharel <aniketkhareldev@gmail.com>
2023-06-16 19:22:47 +0545

    updated font

--- config.def.h
+++ config.def.h
@@ -11,7 +11,7 @@
  * Arch repos and is listed as a dependency for this build. JoyPixels is also
  * a hard dependency and makes colored fonts and emojis possible.
  */
-static char *font = "Iosevka Comfy:pixelsize=20:antialias=true:autohint=true";
+static char *font = "Martian:pixelsize=20:antialias=true:autohint=true";
 static char *font2[] = { 
     "JoyPixels:pixelsize=14:antialias=true:autohint=true",
 };
